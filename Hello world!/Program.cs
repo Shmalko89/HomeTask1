d4588765c8a60f4e818262386e6f6ad8fa669454
Vladimir <shmalko89@list.ru>
2021-02-17 21:22:19 +0400

    Add project files.

---
+++ Hello world!/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Представьтесь пожалуйста!");
+            string name = Console.ReadLine();
+            DateTime thisDay = DateTime.Now;
+            Console.WriteLine($"Приветствую, {name}! Сегодня {thisDay.ToString("D")}");
+        }
+    }
+}
